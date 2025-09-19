e1055cc3db507640a1d96c182b28e0fd38986f08
robinkrv <163308237+robinkrv@users.noreply.github.com>
2025-09-19 09:03:45 +0200

    Initialisation Docker et BDD

---
+++ choral-riff/src/main/java/fr/afpa/choral_riff/ChoralRiffApplication.java
@@ -0,0 +1,13 @@
+package fr.afpa.choral_riff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChoralRiffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChoralRiffApplication.class, args);
+	}
+
+}
---
+++ choral-riff/src/test/java/fr/afpa/choral_riff/ChoralRiffApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.afpa.choral_riff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChoralRiffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
