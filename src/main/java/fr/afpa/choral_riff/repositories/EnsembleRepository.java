363b5859f4857fc46a73f357d18f60cfde053ee9
robinkrv <163308237+robinkrv@users.noreply.github.com>
2025-09-23 16:30:03 +0200

    Repositories

---
+++ src/main/java/fr/afpa/choral_riff/repositories/EnsembleRepository.java
@@ -0,0 +1,8 @@
+package main.java.fr.afpa.choral_riff.repositories;
+
+import fr.afpa.choral_riff.entity.Ensemble;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EnsembleRepository extends JpaRepository<Ensemble, Long> {
+}
+
