1f9373501206e2087ef610875cc15219bb6af28d
KamalAMB <kamal.alimbae@gmail.com>
2025-12-03 10:07:00 +0100

    Ajout fonction du dernier morceau

--- src/main/java/fr/afpa/choral_riff/repositories/MorceauRepository.java
+++ src/main/java/fr/afpa/choral_riff/repositories/MorceauRepository.java
@@ -2,6 +2,8 @@ package fr.afpa.choral_riff.repositories;
 
 import fr.afpa.choral_riff.entity.Morceau;
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,6 +13,6 @@ import org.springframework.stereotype.Repository;
 public interface MorceauRepository extends JpaRepository<Morceau, Long> {
 
     List<Morceau> findByEnsembleId(Long ensembleId);
-    // Optional<Morceau> findTopByOrderByMorceauIdDesc(); 
+    Optional<Morceau> findTopByOrderByMorceauIdDesc(); 
 
 }
