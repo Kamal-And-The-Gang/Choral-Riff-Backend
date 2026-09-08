bcfac0c75e68d31405d96cb1abd5a022b7485f03
nadine35 <72575166+nadine35@users.noreply.github.com>
2026-09-08 14:54:42 +0200

    fix: utiliser le décodage Base64 pour la clé JWT

--- src/main/java/fr/afpa/choral_riff/security/JwtService.java
+++ src/main/java/fr/afpa/choral_riff/security/JwtService.java
@@ -314,8 +314,13 @@ public class JwtService {
                 .getPayload();
     }
 
+    // private SecretKey getSignInKey() {
+    //     byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+    //     return Keys.hmacShaKeyFor(keyBytes);
+    // }
+
     private SecretKey getSignInKey() {
-        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
-        return Keys.hmacShaKeyFor(keyBytes);
-    }
+    byte[] keyBytes = Decoders.BASE64URL.decode(secretKey);
+    return Keys.hmacShaKeyFor(keyBytes);
+}
 }
